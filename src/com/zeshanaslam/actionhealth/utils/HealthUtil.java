343f235c5968e356441aece35a88bb41714682e8
Zeshan Aslam <zeshan12aslam@gmail.com>
2019-12-13 19:13:23 -0500

    1.15 Support

--- src/com/zeshanaslam/actionhealth/utils/HealthUtil.java
+++ src/com/zeshanaslam/actionhealth/utils/HealthUtil.java
@@ -235,7 +235,7 @@ public class HealthUtil {
         message = ChatColor.translateAlternateColorCodes('&', message);
 
         try {
-            if (plugin.configStore.mcVersion.equals("v1_12_R1") || plugin.configStore.mcVersion.startsWith("v1_13") || plugin.configStore.mcVersion.startsWith("v1_14_")) {
+            if (plugin.configStore.mcVersion.equals("v1_12_R1") || plugin.configStore.mcVersion.startsWith("v1_13") || plugin.configStore.mcVersion.startsWith("v1_14_") || plugin.configStore.mcVersion.startsWith("v1_15_")) {
                 new PreAction(player, message);
             } else if (!(plugin.configStore.mcVersion.equalsIgnoreCase("v1_8_R1") || (plugin.configStore.mcVersion.contains("v1_7_")))) {
                 Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + plugin.configStore.mcVersion + ".entity.CraftPlayer");
