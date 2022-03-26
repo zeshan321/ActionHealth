794135b02ea9d3d84e03cfd7984e235be575b629
Zeshan Aslam <zeshan12aslam@gmail.com>
2022-03-26 11:33:22 -0400

    Updated to support MythicMobs 5.0.2. Temp using files directly for MVdWPlaceholderAPI and MythicMobs

--- src/main/java/com/zeshanaslam/actionhealth/support/MythicMobsSupport.java
+++ src/main/java/com/zeshanaslam/actionhealth/support/MythicMobsSupport.java
@@ -1,14 +1,22 @@
 package com.zeshanaslam.actionhealth.support;
 
-import io.lumine.xikage.mythicmobs.MythicMobs;
+import io.lumine.mythic.bukkit.BukkitAPIHelper;
+import io.lumine.mythic.bukkit.MythicBukkit;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 
 public class MythicMobsSupport {
+    private static final MythicBukkit plugin = (MythicBukkit) Bukkit.getServer().getPluginManager().getPlugin("MythicMobs");
+
     public String getMythicName(Entity entity) {
-        if (MythicMobs.inst().getAPIHelper().isMythicMob(entity)) {
-            return MythicMobs.inst().getAPIHelper().getMythicMobInstance(entity).getType().getInternalName();
+        if (plugin == null) {
+            return null;
         }
 
+        BukkitAPIHelper bucketApiHelper = plugin.getAPIHelper();
+        if (bucketApiHelper.isMythicMob(entity)) {
+            return bucketApiHelper.getMythicMobInstance(entity).getType().getInternalName();
+        }
         return null;
     }
 }
